23b1219b4ef0239af1321d5a4f1af31bcb85b441
Cristian <xdinko@gmail.com>
2018-09-20 19:51:24 +0200

    d

--- execute.php
+++ execute.php
@@ -15,12 +15,12 @@ $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name']
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
 $date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "" ;
-$text = $text . $message['from']['first_name'] . $message_id;
+$text = $text . $message['from']['first_name'] . $messageId;
 
 $text = trim($text);
 $text = strtolower($text);
 
 header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => $text, "reply_to_message_id" => $message_id);
+$parameters = array('chat_id' => $chatId, "text" => $text, "reply_to_message_id" => $messageId);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
