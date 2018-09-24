608fb312d6bfb8c54f370ef68c19d0038ce694f8
Cristian Amato <xdinko@gmail.com>
2018-09-24 11:16:45 +0200

    a

---
+++ dbtest.php
@@ -0,0 +1,25 @@
+<?php
+
+
+const DEFAULT_URL = 'https://gw2rbot.firebaseio.com/';
+const DEFAULT_TOKEN = 'AIzaSyD7xGhahp14A9V3EUZe2v_4NU1WHrmYmf0';
+const DEFAULT_PATH = '/firebase/gw2rbot';
+
+$firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
+
+// --- storing an array ---
+$test = array(
+    "foo" => "bar",
+    "i_love" => "lamp",
+    "id" => 42
+);
+$dateTime = new DateTime();
+$firebase->set(DEFAULT_PATH . '/' . $dateTime->format('c'), $test);
+
+// --- storing a string ---
+$firebase->set(DEFAULT_PATH . '/name/contact001', "John Doe");
+
+// --- reading the stored string ---
+$name = $firebase->get(DEFAULT_PATH . '/name/contact001');
+
+?>
